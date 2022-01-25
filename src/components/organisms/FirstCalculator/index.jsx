5d67ab55573ee2fa4b0921f5b3d7844b43698ac2
gilmujjang <alsanrlf@naver.com>
2022-01-25 19:17:52 +0900

    fix : api비동기 에러 처리

--- src/components/organisms/FirstCalculator/index.jsx
+++ src/components/organisms/FirstCalculator/index.jsx
@@ -1,20 +1,33 @@
-import react, { useState } from 'react';
-import caculating, {data} from '../../../utils/Calculate.js'
-import Input from '../../atoms/Input'
+import  { useState,useEffect } from 'react';
+import caculating from '../../../utils/Calculate.js';
 
   function FirstCalculator () {
-    console.log(caculating(2, "KRW"))
-
     const [country, setCountry] = useState("KRW");
-    const [amount, setAmount] = useState("");
     const [price, setPrice] = useState(0);
+    const [data, setData] = useState(null);
+    const [ans, setAns] = useState("");
+
+    useEffect(() => {
+      const Address = "http://api.currencylayer.com/live?access_key=" + process.env.REACT_APP_API_KEY + "&format=1"
+      async function GetApi () {
+        const d = await fetch(Address)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          return myJson.quotes;
+        });
+        setData(d)
+      }
+      GetApi();
+    },[])
 
     const handleSelect = (e) => {
       setCountry(e.target.value);
     }
 
     const OnSubmit =  () => {
-      setAmount(caculating(price, country))
+      setAns(`수취금액은 ${caculating(data, price, country)} 입니다 ${country}`)
     }
 
     const onChange = (e) => {
@@ -22,40 +35,35 @@ import Input from '../../atoms/Input'
     }
 
     return (
-      <>
-        <h1>1st 환율계산 </h1>
-        <div>
+      data ? (
+        <>
+          <h1>1st 환율계산 </h1>
           <div>
-          <span>송금국가: 미국(USD)</span>
+            <div>
+            <span>송금국가: 미국(USD)</span>
+            </div>
+            <div>
+              <span>수취국가:</span>
+              <select onChange ={handleSelect}>
+                <option key="KRW" value="KRW">한국(KRW)</option>
+                <option key="JPY" value="JPY">일본(JPY)</option>
+                <option key="PHP" value="PHP">필리핀(PHP)</option>
+              </select>
+            </div>
+            <div>
+              <span>환율:{Number(data["USD"+country]).toFixed(2)}{country} / USD</span>
+            </div>
+            <div>
+              <span>송금액:</span>
+              <input type="number" id='price' onChange={onChange}/> USD
+            </div>
+            <button type="submit" onClick={OnSubmit} >Submit</button>
           </div>
           <div>
-            <span>수취국가:</span>
-            <select onChange ={handleSelect}>
-              <option key="KRW" value="KRW">한국(KRW)</option>
-              <option key="JPY" value="JPY">일본(JPY)</option>
-              <option key="PHP" value="PHP">필리핀(PHP)</option>
-            </select>
+            {ans}
           </div>
-          <div>
-            <span>환율:{Number(data["USD"+country]).toFixed(2)}{country} / USD</span>
-            
-          </div>
-          <div>
-            <span>송금액:</span>
-            <input type="number" id='price' onChange={onChange}/> USD
-          </div>
-          <button type="submit" onClick={OnSubmit} >Submit</button>
-        </div>
-
-        {amount === '' ? <></> : ( 
-          <div>
-            수취금액은 {amount} USD 입니다.
-          </div>
-        )}
-          
-        
-        
-      </>
+        </>
+      ) : <>no data</>
     )
   }
 
--- src/utils/Calculate.js
+++ src/utils/Calculate.js
@@ -1,9 +1,4 @@
-import GetApi from '../utils/Api'
-
-export const data = GetApi();
-console.log(data);
-
-export default function caculating(input, target){
+export default function caculating(data, input, target){
     const exchageRate = data["USD"+target];
     const output = (exchageRate*input).toFixed(2);
     return output.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
