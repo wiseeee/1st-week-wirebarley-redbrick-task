ec8f95f7ff239786b779688aae3fba9d2cb9b448
wiseeee <yji930714@gmail.com>
2022-01-25 13:33:18 +0900

    feat : FirstCalculator 레이아웃 생성

--- src/utils/Calculate.js
+++ src/utils/Calculate.js
@@ -1,16 +1,15 @@
-
-
 // input => "CAD", "KRW"
 
 // output => "102.47", "1200.01"
 
-const data = {
+export const data = {
     USDKRW: "1121.419945",
-    USDJPY: "110.959498"
+    USDJPY: "110.959498",
+    USDPHP: "51.300504"
 }
 
 export default function caculating(input, target){
     const exchageRate = data["USD"+target];
     const output = (exchageRate*input).toFixed(2);
-    return output;
+    return output.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
