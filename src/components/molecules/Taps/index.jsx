0cec0f526c527b9fd8dcaadcbe0d29e92c24f479
minbr0ther <minbr0ther@naver.com>
2022-01-25 14:01:50 +0900

    feat: 기준일 변환 (unixtime -> KST)

--- src/components/molecules/Taps/index.jsx
+++ src/components/molecules/Taps/index.jsx
@@ -1,61 +1,76 @@
 import React from 'react';
 
 const tabArr = [
-    {
-        tabTitle: 'USD',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    },
-    {
-        tabTitle: 'CAD',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    },
-    {
-        tabTitle: 'KRW',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    },
-    {
-        tabTitle: 'HKD',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    },
-    {
-        tabTitle: 'JPY',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    },
-    {
-        tabTitle: 'CNY',
-        tabContent: (
-            <div>tab1 content</div>
-        )
-    }
-]
+  {
+    tabTitle: 'USD',
+    tabContent: <div>tab1 content</div>,
+  },
+  {
+    tabTitle: 'CAD',
+    tabContent: <div>tab1 content</div>,
+  },
+  {
+    tabTitle: 'KRW',
+    tabContent: <div>tab1 content</div>,
+  },
+  {
+    tabTitle: 'HKD',
+    tabContent: <div>tab1 content</div>,
+  },
+  {
+    tabTitle: 'JPY',
+    tabContent: <div>tab1 content</div>,
+  },
+  {
+    tabTitle: 'CNY',
+    tabContent: <div>tab1 content</div>,
+  },
+];
 
+const numberToMonth = (number) => {
+  const MONTH_TYPE = {
+    1: 'JAN',
+    2: 'FEB',
+    3: 'MAR',
+    4: 'APR',
+    5: 'MAY',
+    6: 'JUN',
+    7: 'JUL',
+    8: 'AUG',
+    9: 'SEP',
+    10: 'OCT',
+    11: 'NOV',
+    12: 'DEC',
+  };
 
-const Taps = ({ currency, result, timestamp }) => (
-    <div>
-        <ul>
-            {
-                tabArr.map((section, index) => 
-                    { 
-                        return (currency === section.tabTitle)
-                        ? <h3 key={index}>{section.tabTitle}</h3>
-                        : <li key={index}>{section.tabTitle}</li>
-                    }
-                )
-            }
-        </ul>
-        <h1>{currency} : {result}</h1>
-        <p>기준일 : {timestamp} </p>
-    </div>
-  );
-  
+  return MONTH_TYPE[number];
+};
+
+const unixTimestamp = (time) => {
+  const timeGapToKST = 9 * 60 * 60 * 1000;
+  const date = new Date(time * 1000 + timeGapToKST);
+  const year = date.getFullYear();
+  const month = numberToMonth(date.getMonth() + 1);
+  const day = '0' + date.getDate();
+  return year + '-' + month + '-' + day.substr(-2);
+};
+
+const Taps = ({ currency, result, mock }) => (
+  <div>
+    <ul>
+      {tabArr.map((section, index) => {
+        return currency === section.tabTitle ? (
+          <h3 key={index}>{section.tabTitle}</h3>
+        ) : (
+          <li key={index}>{section.tabTitle}</li>
+        );
+      })}
+    </ul>
+    <h1>
+      {currency} : {result}
+    </h1>
+    <p>기준일 : {unixTimestamp(mock.timestamp)}</p>
+  </div>
+);
+
 export default Taps;
--- src/components/organisms/SecondCalculator/index.jsx
+++ src/components/organisms/SecondCalculator/index.jsx
@@ -3,14 +3,18 @@ import InputSelect from '../../molecules/InputSelect';
 import Tabs from '../../molecules/Taps';
 
 function SecondCalculator() {
-    const [selected, setSelected] = useState("USD")
-    
-    
+  const [selected, setSelected] = useState('USD');
+  const [mock, setMock] = useState({
+    success: true,
+    terms: 'https://currencylayer.com/terms',
+    privacy: 'https://currencylayer.com/privacy',
+    timestamp: 1643011864,
+  });
 
   return (
     <>
       <InputSelect selected={selected} setSelected={setSelected}></InputSelect>
-      <Tabs currency={selected} />
+      <Tabs currency={selected} mock={mock} />
     </>
   );
 }
