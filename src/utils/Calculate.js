870970feabc9dd920ebfb7763db93dd23025a083
minbr0ther <minbr0ther@naver.com>
2022-01-26 13:39:40 +0900

    fix: custom cors-anywhere 사용

--- src/utils/Calculate.js
+++ src/utils/Calculate.js
@@ -6,6 +6,8 @@ export function Exchange(data, input, target) {
 
 export async function GetJsonData() {
   const Address =
+    'https://sixted-proxy-cors-anyware.herokuapp.com/' +
+    'http://api.currencylayer.com' +
     '/live?access_key=' +
     process.env.REACT_APP_API_KEY +
     '&currencies=CAD,KRW,HKD,JPY,CNY,PHP&source=USD&&format=1';
