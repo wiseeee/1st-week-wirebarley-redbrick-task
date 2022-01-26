b5ddcdd32679d07fe7e01449172958f0fa4906ef
minbr0ther <minbr0ther@naver.com>
2022-01-26 13:23:43 +0900

    fix: api 주소 proxy 세팅

--- src/utils/Calculate.js
+++ src/utils/Calculate.js
@@ -6,8 +6,10 @@ export function Exchange(data, input, target) {
 
 export async function GetJsonData() {
   const Address =
-    'https://cors-anywhere.herokuapp.com/http://api.currencylayer.com/live?access_key=' + process.env.REACT_APP_API_KEY + '&currencies=CAD,KRW,HKD,JPY,CNY,PHP&source=USD&&format=1';
-  const data = await fetch(Address,{mode: "cors"})
+    '/live?access_key=' +
+    process.env.REACT_APP_API_KEY +
+    '&currencies=CAD,KRW,HKD,JPY,CNY,PHP&source=USD&&format=1';
+  const data = await fetch(Address, { mode: 'cors' })
     .then(function (response) {
       return response.json();
     })
